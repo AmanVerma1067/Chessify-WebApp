611701cd26fcf9994867a44e09ed33831b4d1598
aman verma <amanverma1067@gmail.com>
2025-06-14 14:40:51 +0530

    Upgrade the chess engine

--- Backend/bot/stockfish_bot.py
+++ Backend/bot/stockfish_bot.py
@@ -1,11 +1,9 @@
-import chess.engine
+from stockfish import Stockfish
+import os
+
+stockfish = Stockfish(path=os.path.join("stockfish", "stockfish-ubuntu-x86-64"))
 
 def get_stockfish_move(board, time_limit=0.5, elo=1800):
-    try:
-        with chess.engine.SimpleEngine.popen_uci("/usr/games/stockfish") as engine:
-            # adjust the path to Stockfish binary for your server (can be /usr/local/bin/stockfish etc.)
-            result = engine.play(board, chess.engine.Limit(time=time_limit))
-            return result.move
-    except Exception as e:
-        print("Stockfish error:", e)
-        return None
+    stockfish.set_fen_position(board.fen())
+    return chess.Move.from_uci(stockfish.get_best_move())
+
